8477dbe148d74cb50300e3d49e9ecc64a5b21015
Vlado <Vlado@DESKTOP-E5MIU1B>
2026-02-05 09:09:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitTestProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verzija 1.0 - Početak");
+        }
+    }
+}
